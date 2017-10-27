35bdf660b43ac2d4d8fe1d6b6463432bce948e07
keanedawg <cameronfife@hotmail.com>
2017-10-26 21:33:20 -0600

    created hasLetter function and prototyped getnewletters

--- Scrabble/Scrabble/player.cpp
+++ Scrabble/Scrabble/player.cpp
@@ -3,8 +3,20 @@
 
 void Player::printLetters() {
 	cout << "Your letters are: ";
-	for (int i = 0; i < 7; i++) {
+	for (int i = 0; i < letterCount; i++) {
 		cout << myLetters[i] << ' ';
 	}
 	cout << '\n';
+}
+
+bool Player::hasLetter(char letter) {
+	for (int i = 0; i < letterCount; i++) {
+		if (letter == myLetters[i]) {
+			return true;
+		}
+	}
+}
+
+void Player::getNewLetters(int lettersToAdd) {
+
 }
--- Scrabble/Scrabble/player.h
+++ Scrabble/Scrabble/player.h
@@ -7,10 +7,12 @@ class Player {
 public:
 	//void playTurn();
 	void printLetters();
-
+	void getNewLetters(int lettersToGet);
+	bool hasLetter(char letter);
 
 
 private:
+	int letterCount = 7;
 	char myLetters[7] = { 'A', 'S', 'T', 'U', 'D', 'Y', 'G' };
 	int score = 0;
 
